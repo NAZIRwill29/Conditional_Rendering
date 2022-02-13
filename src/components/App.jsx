13193611f8057688de9fd7625a5eed3dce1bae2f
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 09:49:06 +0800

    ternary operator make statement to be useable in render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,11 @@ import Login from "./Login";
 //make islogged in or not
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
-
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    // ternary operator for chg statement into expression
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
 }
 
 export default App;
