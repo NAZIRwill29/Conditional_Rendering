6dac2cb63cbb4d256715a10b039038b3f7a0f0fe
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 09:59:59 +0800

    terniary operator & AND operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,11 @@ var isLoggedIn = false;
 function App() {
   return (
     // ternary operator for chg statement into expression
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    //<div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    //ternary operator with else return null
+    //<div className="container">{isLoggedIn ? <h1>Hello</h1> : null}</div>
+    //&& operator - make if true return  expression and false return null
+    <div className="container">{isLoggedIn && <h1>Hello</h1>}</div>
   );
 }
 
