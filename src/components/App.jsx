123e6df0d03cbbc23d26d81456ec16a4db7a0ea5
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 09:47:28 +0800

    Normal statement for use in render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Login from "./Login";
+
+//make islogged in or not
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
